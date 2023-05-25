0b288d15c40aa7b10a901b63affeb226870d17fb
Dino <brandoncloutier412@gmail.com>
2023-05-25 16:13:45 -0700

    added events

--- discordbot/commands/testing/ping_cmd.js
+++ discordbot/commands/testing/ping_cmd.js
@@ -6,7 +6,8 @@ module.exports = {
         .setDescription("Replies with pong"),
 
     async execute(interaction) {
-        await interaction.reply('Pong!')
-        console.log("hello");
+        await interaction.reply({ content: "hello", ephemeral: true });
+        await interaction.followUp({ content: "second hello", ephemeral: true });
+        await interaction.deleteReply();
     }
 }
