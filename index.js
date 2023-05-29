bab9dbd5f205aef7589aade8596664b05f0899e2
Dino <brandoncloutier412@gmail.com>
2023-05-29 11:36:27 -0700

    Created Server Schemna

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const config = require('./utils/config.js')
 const client = require('./discordbot/discordapp.js')
+const mongoose = require("mongoose");
 
+mongoose.set("strictQuery", false);
+
+// Connecting to database
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("error connection to MongoDB:", error.message);
+  });
+
+
+// Connecting Discord Bot
 client.login(config.DISC_TOKEN)
