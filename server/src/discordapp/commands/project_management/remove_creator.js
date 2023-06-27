c0aa62eb5c56af538bf83e6f5975295003dd83c7
Dino <brandoncloutier412@gmail.com>
2023-06-27 15:46:58 -0700

    finished remove_creator command

--- server/src/discordapp/commands/project_management/remove_creator.js
+++ server/src/discordapp/commands/project_management/remove_creator.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const Project = require("../../../../models/project");
 const Member = require("../../../../models/member");
 
@@ -20,7 +20,7 @@ module.exports = {
         const inputUserId = interaction.options.getUser("user").id;
         try {
             if (cmdUserId === inputUserId) return await interaction.editReply("You cannot perform this command on yourself")
-            
+
             // @GUARD: Make sure the command is executed inside a category
             if (!categoryId) return await interaction.editReply("You can only execute this command inside a node");
 
@@ -31,49 +31,33 @@ module.exports = {
             if (!project) return await interaction.editReply("You can only execute this command inside a node");
 
             // @GUARD: Check to make sure the user is a manager in the project
-            const manager = project.managers.find((manager) => {
-                return manager.discordId === cmdUserId;
-            });
-
-            if (!manager) return await interaction.editReply("You do not have the permissions to perform this command");
-
+            if (!project.managers.find((manager) => manager.discordId === cmdUserId)) return await interaction.editReply("You do not have the permissions to perform this command");
+            
             // @GUARD: Check to see if the inputed user is a manager, another manager cannot kick another manager
             if (project.managers.find((manager) => manager.discordId === inputUserId))
                 return await interaction.editReply("This user is a manager. Please use the /remove_manager command then use the /remove_creator command");
 
-            // Okay now we know the command user is a manager and the inputted user is not a manager. We can now update the project members array so it doesnt include the inputted user and then we need to remove the project from the users project array.
-
-            let userFound;
+            // Remove the user from the members array
+            let userToRemove;
             project.members = project.members.filter((member) => {
                 if (member.discordId !== inputUserId) return true;
 
-                userFound = member;
+                userToRemove = member;
                 return false;
             })
 
-            console.log(userFound)
-            //@GUARD: Check to see if the use is a
-            // // @GUARD: Check to see if the user is already a creator
-            // if (project.members.find((member) => member.discordId === inputUserId)) return await interaction.editReply("This user is already a creator")
+            // @GUARD: The user is not a creator
+            if (!userToRemove) return await interaction.editReply("This user is not currently a creator")
+            userToRemove.projects = userToRemove.projects.filter((project) => project._id !== project._id);
 
-            // // Grab the new creators profile from the database
-            // const newCreator = await Member.findOne({ discordId: inputUserId }).populate("projects");
+            await project.save();
+            await userToRemove.save();
 
-            // // @GUARD: Check to make sure the user has a profile in the database
-            // if (!newCreator) return await interaction.editReply("This user has yet to create a profile")
+            // Remove the roles from the user
+            const newManagerDiscObj = await interaction.guild.members.fetch(inputUserId);
+            newManagerDiscObj.roles.remove(project.roles[1].id) // Creator Role
 
-            // // Add the user as a creator in the projects database
-            // project.members.push(newCreator._id);
-            // newCreator.projects.push(project._id);
-            // await project.save();
-            // await newCreator.save();
-
-            // // Give the user the roles in the discord server
-            // const newManagerDiscObj = await interaction.guild.members.fetch(inputUserId);
-            // newManagerDiscObj.roles.add(project.roles[1].id) // Creator Role
-            // newManagerDiscObj.roles.remove(project.roles[2].id) // Viewing Role
-
-            // await interaction.editReply(`<@${inputUserId}> has been added as a creator for the ${project.name} node`)
+            return await interaction.editReply(`<@${inputUserId}> has been removed as a creator from the ${project.name} node`)
         } catch (error) {
             console.log(error);
             await interaction.editReply({
