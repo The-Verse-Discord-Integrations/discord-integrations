7eb3b78950d61582eea62437d2398e50f3098eac
Dino <brandoncloutier412@gmail.com>
2023-07-01 15:31:56 -0700

    added comments of some changes and errors that can occur

--- server/src/discordapp/commands/project_management/transfer_owner.js
+++ server/src/discordapp/commands/project_management/transfer_owner.js
@@ -35,9 +35,11 @@ module.exports = {
 
         if (!newOwnProfile) return await interaction.editReply("This user has yet to create a profile") //checking if new owner has profile
 
-
-        //adding newOwner to members array is not already there
-        if (project.members.find((member) => member.discordId !== newOwner)) {
+        // @ERROR: The if statement condition will be true if the .find method returns back an object. Since for the .find condition you are looking for a member.discordId !== newOwner it will always return back true returning the wrong member object
+        // @Suggestion: The .find method will return undefined if the element is not found in the members array. So you can combine this with a not operator (!) to create a condition that looks like this if(!project.members.find()).
+        // This will first look through the project.members array and find a member that matches the id. If there isn't one meaning the .find method returned undefined the condition will evaluate to true adding the member to the project because of the not operator (!)
+        // adding newOwner to members array is not already there
+        if (project.members.find((member) => member.discordId !== newOwner)) { 
 
             project.members.push(newOwnProfile._id);
             newOwnProfile.projects.push(project._id);
@@ -45,6 +47,9 @@ module.exports = {
             await newOwnProfile.save();
         }
 
+        // @ERROR: project.owner is actually a variable that holds a reference to a document (member) in the members collection (in mongodb)
+        // We should just be able to reassign the variable to the new owner object._id which would look like this:
+        // project.owner = newOwnProfile._id
         //change project owner ID to newOwnProfile Id. project.owner.id = newOwnProfile ID
         if (project.owner.find((owner) => owner.discordId !== newOwner)) {
 
@@ -52,19 +57,21 @@ module.exports = {
         }
 
 
-
-
         //adding new owner to managers array
+        // @HERE: Since the new owner could already be a manager we also want to check if they are inside the managers array using the index.of method.
         project.managers.push(newOwnProfile._id);
-        project.members.indexOf(newManager._id) === -1 && project.members.push(newManager._id)
+        // project.members.indexOf(newManager._id) === -1 && project.members.push(newManager._id) // @DELETE
         project.save();
 
         // give new owner manager role and take away viewer and creative 
+        // @CHANGE: change the newManagerDiscObj to newOwnProfile
         const newManagerDiscObj = await interaction.guild.members.fetch(inputUserId);
         newManagerDiscObj.roles.add(project.roles[0].id) // Manager Role
         newManagerDiscObj.roles.remove(project.roles[1].id) // Creator Role
         newManagerDiscObj.roles.remove(project.roles[2].id) // Viewing Role
 
+
+        // @HERE: Since we want to touch the database as little as possible we could move the project.save() on line 46 out here so it is only saved once
 
         await interaction.editReply(`<@${newOwner}> has been tranferred ownership for the ${project.name} node`)
 
