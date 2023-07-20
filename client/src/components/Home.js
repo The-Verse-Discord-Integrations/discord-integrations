54ed2f9c1f881447f4f0a8252a2200b815b463a4
Brandon Cloutier <dino@Brandons-MacBook-Pro.local>
2023-07-20 13:46:00 -0700

    created profile page for web app

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, createContext } from 'react';
 import { useNavigate, useLoaderData, Outlet } from "react-router-dom";
 import axios from 'axios';
+
+export const UserContext = createContext(null);
 
 const Home = () => {
     const [profile, setProfile] = useState(null);
@@ -23,10 +25,12 @@ const Home = () => {
         getProfile()
     }, [navigate])
     return (
-        <div className="flex flex-col h-full">
-            <Nav />
-            <Outlet />
-        </div>
+        <UserContext.Provider value={profile} >
+            <div className="flex flex-col h-full">
+                <Nav />
+                <Outlet />
+            </div>
+        </UserContext.Provider>
     )
 }
 
@@ -36,23 +40,23 @@ const Nav = () => {
     return (
         <nav className="border-b bg-white">
             <div className="flex justify-between py-2 px-3  items-center container mx-auto">
-                <header className="flex items-center">
+                <header className="flex items-center hover:cursor-pointer" onClick={() => navigate('/')}>
                     <img className="h-10" src="https://media.licdn.com/dms/image/C560BAQGZsRf5Ro6Zbg/company-logo_200_200/0/1631752036761?e=2147483647&v=beta&t=kVWNkg1BQJDVWm4UhD8L8OmOLxhx_xc2Kc_-V_hC7DQ" alt="" />
                     <span className="px-2 font-medium text-xl tracking-tighter">The Verse</span>
                 </header>
                 <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="rgb(216, 218, 221)"
-                        className="bi bi-person-circle h-10 hover:cursor-pointer"
-                        viewBox="0 0 16 16"
-                        onClick={() => navigate('/profile')}
-                    >
-                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                        <path
-                            fillRule="evenodd"
-                            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-                        />
-                    </svg>
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="rgb(216, 218, 221)"
+                    className="bi bi-person-circle h-10 hover:cursor-pointer"
+                    viewBox="0 0 16 16"
+                    onClick={() => navigate('/profile')}
+                >
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                    <path
+                        fillRule="evenodd"
+                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                    />
+                </svg>
             </div>
         </nav>
     )
