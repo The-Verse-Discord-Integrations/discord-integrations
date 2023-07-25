0125f27a05070d28cdcce168e7eedaeb7758550e
enricak <enricakumar@gmail.com>
2023-07-24 22:07:56 -0400

    Finished event handler function. need to fix edge cases still

--- server/src/discordapp/events/client/message.js
+++ server/src/discordapp/events/client/message.js
@@ -10,19 +10,11 @@ module.exports = {
 
             const hashMapKey = (Math.floor((interaction.createdTimestamp + 345600000) / 604800000)) //Hashmap key represents week count
             const dayIndex = new Date(interaction.createdTimestamp).getUTCDay() //hashmap subkey is array with 7 indexes, each corresponding to day of the week
-            const weeklyMessageCount =0; //Calculated at end 
+            const weeklyMessageCount = 0;  
 
             const member = await Member.findOne({ discordId: interaction.author.id }).populate('projects').populate(messageCount)
 
-            const channel = await client.channels.fetch(interaction.channelId)
-
-            const messageCount = new Member({
-                hashMapField: {
-                  key1: hashMapKey,
-                  key2: [0, 0, 0, 0, 0, 0, 0],
-                  key3: weeklyMessageCount,
-                }
-              });
+            const channel = await client.channels.fetch(interaction.channelId)  //needed?
 
             
 
@@ -36,17 +28,18 @@ module.exports = {
                       weeklyMessageCount: 1,
                     },
                   };
-                member.messageCount.hashMapKey. = hashMapKey;
-                member.messageCount.key2 = [0, 0, 0, 0, 0, 0, 0];
-                member.messageCount.key3 = weeklyMessageCount;
+                member.messageCount.hashMapKey.dayCount[dayIndex]++;
+                member.messageCount.hashMapKey.weeklyMessageCount++;
 
-                member.save;
             } else {
-                member.messageCount.key2.[dayIndex]++;
-                member.messageCount.
+                member.messageCount.hashMapKey.dayCount[dayIndex]++;
+                member.messageCount.hashMapKey.weeklyMessageCount++;
             }
 
-            if (member.projects.find(project => project.categoryId === channel.parentId)) {
+            member.save();
+
+         /*   OLD IMPLENTATION::
+         if (member.projects.find(project => project.categoryId === channel.parentId)) {
                 // Check to see if the message count is set for the user, if it is then increase the count.
                 if (!member.weeklyMessageCount) {
                     member.weeklyMessageCount = 1;
@@ -58,6 +51,9 @@ module.exports = {
             console.log(Math.floor((interaction.createdTimestamp + 345600000) / 604800000))
             console.log((Math.floor(interaction.createdTimestamp / 86400000) + 4) % 7)
             console.log(new Date(interaction.createdTimestamp).getUTCDay())
+
+        */
+
         } catch (error) {
             console.log(error)
         }
