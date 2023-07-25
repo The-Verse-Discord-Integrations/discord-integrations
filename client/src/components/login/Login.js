969af081567d810cc128f75a22d1ff6b90e67076
bcloutier412 <brandoncloutier412@gmail.com>
2023-07-25 10:21:51 -0700

    edited ui

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,20 +1,18 @@
-import LoginImage from './images/image.png'
-import LoginVideo from './images/backgroundVideo.mp4'
-
 const Login = () => {
 
     return (
         <div className="h-full flex flex-row relative overflow-hidden bg-cover" style={{ backgroundImage: `url(https://i.pinimg.com/originals/24/cc/eb/24cceb9cdcb03120898e70e2b42fc37d.jpg)` }}>
             <div className="grow relative w-1/2 xl:block hidden">
-                <img className="w-32 absolute top-10 left-10" src={require('./images/VerseLogoAsset.png')} alt="" />
+                <img className="w-24 absolute top-10 left-10" src={require('./images/VerseLogoAsset.png')} alt="" />
+                <div className="text-white font-semibold text-2xl tracking-tight m-52">Lost In Liminal Space</div>
             </div>
-            <div className="grow flex items-center justify-center w-1/2 flex-col text-center bg-[rgba(0,0,0,.2);]">
-                <img className="w-32 xl:hidden" src={require('./images/VerseLogoAsset.png')} alt="" />
-                <section className="my-10 xl:px-10">
-                    <div className="xl:text-5xl text-2xl text-white font-bold mb-5">YOU'RE ALMOST THERE...</div>
-                    <div className="xl:text-xl text-xl text-white px-10">Login with your discord to gain access to your member portal where you can access </div>
-                </section>
-                <a className="text-white border border-white rounded px-5 " href="http://localhost:3000/auth">Login With Discord</a>
+            <div className="grow flex items-center xl:justify-center w-1/2 flex-col xl:bg-[rgba(0,0,0,.5);] bg-gradient-to-t from-[rgba(0,0,0,.8)] to-[rgba(0,0,0,.2)]">
+                <div className="flex flex-col items-center xl:mt-0 xl:justify-center mt-36 gap-5 text-center">
+                    <img className="w-24 xl:hidden" src={require('./images/VerseLogoAsset.png')} alt="" />
+                    <div className="text-white xl:text-5xl text-3xl">Your Journey Starts Here</div>
+                    <div className="text-white max-w-[450px] mx-5 pb-5 border-b border-white/[.3]">Login with your discord to gain access to your member portal</div>
+                    <a className="text-white bg-[#292f5e] rounded px-14 py-1.5 flex items-center gap-4" href="http://localhost:3000/auth"><img className="w-10 inline" src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png" alt=""/>Login With Discord</a>
+                </div>
             </div>
         </div>
     )
