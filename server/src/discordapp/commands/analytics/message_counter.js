ec5bc3df9a4751061de527a6b31f2b2df40da492
enricak <enricakumar@gmail.com>
2023-07-31 23:26:46 -0400

    Initialized file and started message count command

---
+++ server/src/discordapp/commands/analytics/message_counter.js
@@ -0,0 +1,54 @@
+const Discord = require('discord.js');
+const { ChannelType, SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const Member = require('./member.js'); //importing user schema to get username, messages, and last messagedate, but need to update somehow?
+const Project = require('../../../../models/project');
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('weekly_messages')
+        .setDescription('See how many messages a user sent this week')
+        .addUserOption((option) => option.setName("user").setDescription("Who's message count do you want to see?").setRequired(true))
+        .addBooleanOption((option) => option.setName("confirmation").setDescription("Please confirm this command").setRequired(true)),
+    
+        async execute(interaction) {
+        if (!interaction.options.getBoolean("confirmation")) return await interaction.reply({ content: "Please confirm command", ephemeral: true })
+        await interaction.deferReply({ ephemeral: true })
+            
+        const targetUser = interaction.options.getUser('user').id
+
+
+
+        try{
+
+
+
+        
+                const messageCount = Member.weeklyMessageCount;
+                
+                await interaction.editReply(`<@${targetUser}> has sent ${messageCount} messages this week.`)
+                                        
+            }      
+        }
+        
+        /*message.guild.channels.cache.forEach(channel => {
+  channel.messages.fetch().then(messages => {
+    messages.forEach(msg => console.log(msg.content));
+  });
+}); */
+
+        } catch (error) {
+            console.log(error)
+            await interaction.editReply({
+                embeds: [
+                    new EmbedBuilder({
+                        title: `❌ ERROR OCCURED ❌`,
+                        description: "Reverting Process",
+                        color: 16711680,
+                    }),
+                ],
+            });
+        }
+    }
+}
+
