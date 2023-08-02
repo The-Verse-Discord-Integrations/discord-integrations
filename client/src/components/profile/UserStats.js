e92d09a0edfba982647674b60710480a06aa36af
bcloutier412 <brandoncloutier412@gmail.com>
2023-08-02 10:08:36 -0700

    changed ui for chart

--- client/src/components/profile/UserStats.js
+++ client/src/components/profile/UserStats.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 ChartJS.register(
@@ -13,13 +13,25 @@ ChartJS.register(
 const UserStats = ({ profile }) => {
 
     return (
-        <div className="container relative mx-auto bg-white xl:border-b-0 border-b border-slate-100 xl:shadow xl:rounded flex xl:flex-row flex-col justify-center w-full py-10 pr-10 pl-5">
+        <div className="container relative mx-auto bg-white xl:border-b-0 border-b border-slate-100 xl:shadow xl:rounded flex xl:flex-row flex-col justify-center w-full xl:py-10 xl:pr-10 xl:pl-5">
             <WeeklyBarChart profile={profile}/>
         </div>
     )
 }
 
 const WeeklyBarChart = ({profile}) => {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            setWindowWidth(window.innerWidth)
+            console.log(window.innerWidth)
+        })
+        return () => window.addEventListener("resize", () => {
+            setWindowWidth(window.innerWidth)
+        })
+    })
+
     const options = {
         responsive: true,
         aspectRatio: 1 | 2,
@@ -33,18 +45,24 @@ const WeeklyBarChart = ({profile}) => {
             x: {
                 grid: {
                     drawOnChartArea: false
-                }
+                },
+                ticks: {
+                    display: windowWidth > 425 ? true : false,
+                    font: {
+                        size: windowWidth > 1024 ? 10 : windowWidth >= 768 ? 8 : 0
+                    }
+                },
             },
             y: {
                 title: {
-                    display: true,
-                    text: "Messages",
-                    font: {
-                        size: 24
-                    }
-                }
+                    display: windowWidth >= 1024 ? true : false,
+                    text: windowWidth >= 1024 ? "Messages" : '',
+                    font: windowWidth >= 1024 ? {size: 24} : null
+                },
+                grace: '5%'
             }
-        }
+        },
+        barPercentage: .7,
     };
 
     const labels = []
@@ -55,10 +73,10 @@ const WeeklyBarChart = ({profile}) => {
     for (let i = 0; i < 12; i++) {
         const index = messageData.length - 1 - i
         if (messageData[index]) {
-            labels.unshift(getWeekString(messageData[index][0]))
+            labels.unshift(getWeekString(messageData[index][0], windowWidth))
             messageDataFormatted.unshift(messageData[index][1])
         } else {
-            labels.unshift(getWeekString(currentWeekNum - i))
+            labels.unshift(getWeekString(currentWeekNum - i, windowWidth))
             messageDataFormatted.unshift(0)
         }
     }
@@ -77,14 +95,14 @@ const WeeklyBarChart = ({profile}) => {
     return <Bar options={options} data={data} />
 }
 
-const getWeekString = (unixWeek) => {
+const getWeekString = (unixWeek, windowWidth) => {
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     unixWeek = parseInt(unixWeek)
 
     const weekStart = new Date((unixWeek * 604800000) - 259200000)
     const weekEnd = new Date(weekStart.getTime() + 518400000)
 
-    return `${months[weekStart.getMonth()]} ${weekStart.getDate()} - ${months[weekEnd.getMonth()]} ${weekEnd.getDate()}`
+    return `${windowWidth >= 1024 ? months[weekStart.getMonth()] : weekStart.getMonth() + 1 + '/'}${windowWidth >= 1024 ? ' ' : '/'}${weekStart.getDate()} - ${windowWidth >= 1024 ? months[weekEnd.getMonth()] : weekStart.getMonth() + 1 + '/'}${windowWidth >= 1024 ? ' ' : '/'}${weekEnd.getDate()}`
 }
 
 export default UserStats
