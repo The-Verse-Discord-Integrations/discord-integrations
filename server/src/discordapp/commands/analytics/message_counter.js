5e48937b0599fc7d7510e1068481356f5776bbe6
bcloutier412 <brandoncloutier412@gmail.com>
2023-08-04 10:55:55 -0700

    removed the confirmation and formatted the js file

--- server/src/discordapp/commands/analytics/message_counter.js
+++ server/src/discordapp/commands/analytics/message_counter.js
@@ -8,25 +8,23 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('weekly_messages')
         .setDescription('See how many messages a user sent this week')
-        .addUserOption((option) => option.setName("user").setDescription("Who's message count do you want to see?").setRequired(true))
-        .addBooleanOption((option) => option.setName("confirmation").setDescription("Please confirm this command").setRequired(true)),
-    
-        async execute(interaction) {
-        if (!interaction.options.getBoolean("confirmation")) return await interaction.reply({ content: "Please confirm command", ephemeral: true })
+        .addUserOption((option) => option.setName("user").setDescription("Who's message count do you want to see?").setRequired(true)),
+
+    async execute(interaction) {
         await interaction.deferReply({ ephemeral: true })
-            
+
         const inputUser = interaction.options.getUser('user').id
         //Find the day and week references that the slash command was sent
         const currDay = new Date(interaction.createdTimestamp).getUTCDay();
         const currWeek = (Math.floor((interaction.createdTimestamp + 345600000) / 604800000)).toString();
 
 
-        
-        try{
-            
+
+        try {
+
             //Find inputUser's member profile
             const targetUser = await Member.findOne({ discordId: inputUser }).populate('weeklyMessageCount')
-            
+
             //Edge case if targetUser does not have a user profile
             if (!targetUser) return await interaction.editReply("This user has yet to create a profile")
 
@@ -34,15 +32,15 @@ module.exports = {
             if (!targetUser.weeklyMessageCount || !targetUser.weeklyMessageCount.has(currWeek)) {
                 return await interaction.editReply(`<@${inputUser}> has not sent any messages this week.`);
             }
-            
+
             //Get message counts for week and current day 
             const currWeekCount = targetUser.weeklyMessageCount.get(currWeek).get('totalCount');
             const currDayCount = targetUser.weeklyMessageCount.get(currWeek).get('dailyCount')[currDay];
 
             await interaction.editReply(`<@${inputUser}> has sent ${currDayCount} messages today, with a total of ${currWeekCount} messages this week.`)
-                                             
 
-        
+
+
 
 
         } catch (error) {
